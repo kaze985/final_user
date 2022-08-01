09cd4f6022c06a74a8cf79321d0c03be31dae3c9
kaze8888 <2082856592@qq.com>
2022-08-01 19:42:01 +0800

    update

---
+++ shared.all/user.shared/user.service.impl/src/main/java/com/geekgame/demo/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.geekgame.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    // 设置跨域访问
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "TRACE")
+                .allowCredentials(true);
+    }
+}
